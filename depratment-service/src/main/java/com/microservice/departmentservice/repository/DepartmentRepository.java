da43fcaa934c5cef7fcaa11e3e9231be53bd7aab
madhu179 <madhubabu232343@gmail.com>
2023-12-17 11:35:34 +0530

    Adding code for department service

---
+++ depratment-service/src/main/java/com/microservice/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.microservice.departmentservice.repository;
+
+import com.microservice.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentCode(String departmentCode);
+}
---
+++ depratment-service/src/main/java/com/microservice/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,39 @@
+package com.microservice.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import com.microservice.departmentservice.dto.DepartmentDto;
+import com.microservice.departmentservice.entity.Department;
+import com.microservice.departmentservice.mapper.DepartmentMapper;
+import com.microservice.departmentservice.repository.DepartmentRepository;
+import com.microservice.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+
+        // convert department dto to department jpa entity
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        DepartmentDto savedDepartmentDto = DepartmentMapper.mapToDepartmentDto(savedDepartment);
+
+        return savedDepartmentDto;
+    }
+
+    @Override
+    public DepartmentDto getDepartmentByCode(String departmentCode) {
+
+        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+
+        DepartmentDto departmentDto = DepartmentMapper.mapToDepartmentDto(department);
+
+        return departmentDto;
+    }
+}
