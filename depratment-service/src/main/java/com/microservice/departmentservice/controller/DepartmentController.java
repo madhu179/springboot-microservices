b0df0542748516bedc9f8b03fe2ad7c907bf6d81
madhu179 <madhubabu232343@gmail.com>
2023-12-17 11:44:26 +0530

    Adding small change in controller

--- depratment-service/src/main/java/com/microservice/departmentservice/controller/DepartmentController.java
+++ depratment-service/src/main/java/com/microservice/departmentservice/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/api/departments/")
 @AllArgsConstructor
 public class DepartmentController {
 
