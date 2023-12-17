5fff4f50309a393ef6c3c770424996810fb65393
madhu179 <madhubabu232343@gmail.com>
2023-12-17 12:07:25 +0530

    Adding code for employee service

---
+++ employee-service/src/main/java/com/microservice/employeeservice/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.microservice.employeeservice.service;
+
+import com.microservice.employeeservice.dto.APIResponseDto;
+import com.microservice.employeeservice.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto saveEmployee(EmployeeDto employeeDto);
+
+    APIResponseDto getEmployeeById(Long employeeId);
+}
---
+++ employee-service/src/main/java/com/microservice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,68 @@
+package com.microservice.employeeservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import com.microservice.employeeservice.dto.APIResponseDto;
+import com.microservice.employeeservice.dto.DepartmentDto;
+import com.microservice.employeeservice.dto.EmployeeDto;
+import com.microservice.employeeservice.entity.Employee;
+import com.microservice.employeeservice.mapper.EmployeeMapper;
+import com.microservice.employeeservice.repository.EmployeeRepository;
+
+import com.microservice.employeeservice.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee saveDEmployee = employeeRepository.save(employee);
+
+        EmployeeDto savedEmployeeDto = EmployeeMapper.mapToEmployeeDto(saveDEmployee);
+
+        return savedEmployeeDto;
+    }
+
+    @Override
+    public APIResponseDto getEmployeeById(Long employeeId) {
+
+        LOGGER.info("inside getEmployeeById() method");
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+//        apiResponseDto.setDepartment(departmentDto);
+//        apiResponseDto.setOrganization(organizationDto);
+        return apiResponseDto;
+    }
+
+    public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+
+        LOGGER.info("inside getDefaultDepartment() method");
+
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("R&D Department");
+        departmentDto.setDepartmentCode("RD001");
+        departmentDto.setDepartmentDescription("Research and Development Department");
+
+        EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
+
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployee(employeeDto);
+        apiResponseDto.setDepartment(departmentDto);
+        return apiResponseDto;
+    }
+}
