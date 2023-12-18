fc7a6a9c7f40070e395156dbd37c7d871d0cbf2b
madhu179 <madhubabu232343@gmail.com>
2023-12-18 08:28:30 +0530

    Adding code for spring web flux for WEB CLient instead of rest template

--- employee-service/src/main/java/com/microservice/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/microservice/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -20,7 +21,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -39,8 +41,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         LOGGER.info("inside getEmployeeById() method");
         Employee employee = employeeRepository.findById(employeeId).get();
-        DepartmentDto departmentDto = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(),
-                DepartmentDto.class).getBody();
+//        DepartmentDto departmentDto = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(),
+//                DepartmentDto.class).getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
 
