e015ab93bc9d02d814a92bbd5a6f43602416baba
madhu179 <madhubabu232343@gmail.com>
2024-01-13 18:29:58 +0530

    Adding general api info with annotations

--- depratment-service/src/main/java/com/microservice/departmentservice/DepratmentServiceApplication.java
+++ depratment-service/src/main/java/com/microservice/departmentservice/DepratmentServiceApplication.java
@@ -1,8 +1,34 @@
 package com.microservice.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service Rest APIs",
+				description = "Department Service Rest APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Dev",
+						email = "dev@gmail.com",
+						url = "https://www.devvw.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.devvw.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc",
+				url = "https://www.devvw.com"
+		)
+)
 @SpringBootApplication
 public class DepratmentServiceApplication {
 
