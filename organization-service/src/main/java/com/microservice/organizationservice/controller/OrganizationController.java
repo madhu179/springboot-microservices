072e43de78505061599d19f7116442ec10c99688
madhu179 <madhubabu232343@gmail.com>
2024-01-16 13:48:06 +0530

    Adding code for customizing model schema and api controller organization

--- organization-service/src/main/java/com/microservice/organizationservice/controller/OrganizationController.java
+++ organization-service/src/main/java/com/microservice/organizationservice/controller/OrganizationController.java
@@ -2,11 +2,18 @@ package com.microservice.organizationservice.controller;
 
 import com.microservice.organizationservice.dto.OrganizationDTO;
 import com.microservice.organizationservice.service.OrganizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Organization Service - Organization Controller",
+        description = "Organization Controller Exposes REST APIs for Organization-Service"
+)
 @RestController
 @RequestMapping("api/organizations/")
 @AllArgsConstructor
@@ -14,6 +21,14 @@ public class OrganizationController {
 
     private OrganizationService organizationService;
 
+    @Operation(
+            summary = "Save Organization REST API",
+            description = "Save Organization REST API is used to save the organization object to db"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     // Build Save Organization REST API
     @PostMapping
     public ResponseEntity<OrganizationDTO> saveOrganization(@RequestBody OrganizationDTO organizationDTO){
@@ -21,6 +36,14 @@ public class OrganizationController {
         return new ResponseEntity<>(savedOrganization, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Organization REST API",
+            description = "Get Organization REST API is used to get the organization object from db"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     // Build Get Organization by Code REST API
     @GetMapping("{code}")
     public ResponseEntity<OrganizationDTO> getOrganization(@PathVariable("code") String organizationCode){
--- organization-service/src/main/java/com/microservice/organizationservice/dto/OrganizationDTO.java
+++ organization-service/src/main/java/com/microservice/organizationservice/dto/OrganizationDTO.java
@@ -1,5 +1,6 @@
 package com.microservice.organizationservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,14 +8,21 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(
+        description = "OrganizationDTO Model Information"
+)
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrganizationDTO {
     private Long id;
+    @Schema(description = "Organization Name")
     private String organizationName;
+    @Schema(description = "Organization Description")
     private String organizationDescription;
+    @Schema(description = "Organization Code")
     private String organizationCode;
+    @Schema(description = " Organization Created Date")
     private LocalDateTime createdDate;
 }
