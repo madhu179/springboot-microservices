c3a3e44b56ac138ee3c9f93741be8032ec64011d
madhu179 <madhubabu232343@gmail.com>
2024-01-16 14:09:31 +0530

    Adding code to customize swagger for employee service

--- employee-service/src/main/java/com/microservice/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/microservice/employeeservice/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.microservice.employeeservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import com.microservice.employeeservice.dto.APIResponseDto;
 import com.microservice.employeeservice.dto.EmployeeDto;
@@ -8,6 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name="Employee Service - Employee Controller",
+        description = "Employee Controller Exposes REST APIs for Employee-Service"
+)
 @RestController
 @RequestMapping("api/employees/")
 @AllArgsConstructor
@@ -15,6 +22,14 @@ public class EmployeeController {
 
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "Save Employee REST API",
+            description = "Save Employee REST API is used to save the employee object to db"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     // Build Save Employee REST API
     @PostMapping
     public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
@@ -22,6 +37,14 @@ public class EmployeeController {
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get Employee REST API",
+            description = "Get Employee REST API is used to get the employee object from db"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     // Build Get Employee REST API
     @GetMapping("{id}")
     public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId){
