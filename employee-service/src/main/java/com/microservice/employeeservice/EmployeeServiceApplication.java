74b2d3278a5ab0ace724a2e2f76c2daa24ccd8c1
madhu179 <madhubabu232343@gmail.com>
2024-01-16 13:52:38 +0530

    Adding code for swagger to employee service and adding general information

--- employee-service/src/main/java/com/microservice/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/microservice/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,10 @@
 package com.microservice.employeeservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,6 +12,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Service Rest APIs",
+				description = "Employee Service Rest APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Dev",
+						email = "dev@gmail.com",
+						url = "https://www.devvw.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.devvw.com/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Employee-Service Doc",
+				url = "https://www.devvw.com/Employee-Service.html"
+		)
+)
 @SpringBootApplication
 @EnableFeignClients
 public class EmployeeServiceApplication {
